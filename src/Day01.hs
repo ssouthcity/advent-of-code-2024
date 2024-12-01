b983b4a34145f813b5f86b7dc4bc484fe1b16f1f
Stian Sørby <51554341+ssouthcity@users.noreply.github.com>
2024-12-01 15:49:38 +0100

    feat: completed day 1 puzzle

--- src/Day01.hs
+++ src/Day01.hs
@@ -1,7 +1,23 @@
 module Day01 where
 
+import Data.List (group, sort)
+import Data.Maybe (fromMaybe)
+
+parseInput :: String -> ([Int], [Int])
+parseInput input =
+  let matrix = map (map read . words) (lines input)
+      pairs = [(x, y) | [x, y] <- matrix]
+   in unzip pairs
+
 solvePart1 :: String -> String
-solvePart1 input = "unimplemented"
+solvePart1 input =
+  let (left, right) = parseInput input
+      diffs = zipWith (-) (sort left) (sort right)
+   in show $ sum $ map abs diffs
 
 solvePart2 :: String -> String
-solvePart2 input = "unimplemented"
+solvePart2 input =
+  let (left, right) = parseInput input
+      occurances = map (\xs -> (head xs, length xs)) $ group $ sort right
+      weightedOccurance x = x * fromMaybe 0 (lookup x occurances)
+   in show $ sum $ map weightedOccurance left
--- test/Day01Spec.hs
+++ test/Day01Spec.hs
@@ -3,16 +3,27 @@ module Day01Spec (tests) where
 import qualified Day01
 import Test.HUnit
 
+exampleList :: String
+exampleList =
+  unlines
+    [ "3   4",
+      "4   3",
+      "2   5",
+      "1   3",
+      "3   9",
+      "3   3"
+    ]
+
 testPart1 :: Test
 testPart1 =
-  let input = ""
-      answer = "unimplemented"
+  let input = exampleList
+      answer = "11"
    in TestCase $ assertEqual "" answer $ Day01.solvePart1 input
 
 testPart2 :: Test
 testPart2 =
-  let input = ""
-      answer = "unimplemented"
+  let input = exampleList
+      answer = "31"
    in TestCase $ assertEqual "" answer $ Day01.solvePart2 input
 
 tests :: Test
