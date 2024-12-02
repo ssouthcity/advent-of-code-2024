c789e97c206e7d5c0126c1b515cc872f27a694d0
Stian Sørby <51554341+ssouthcity@users.noreply.github.com>
2024-12-02 23:50:15 +0100

    feat: completed day 2 puzzle

--- src/Day02.hs
+++ src/Day02.hs
@@ -1,7 +1,22 @@
 module Day02 where
 
+parseInput :: String -> [[Int]]
+parseInput input = map (map read . words) (lines input)
+
+isSafe :: [Int] -> Bool
+isSafe xs =
+  let diffs = zipWith (-) xs (tail xs)
+      inRange x = x >= 1 && x <= 3
+      allSorted = all (> 0) diffs || all (< 0) diffs
+   in allSorted && all (inRange . abs) diffs
+
+removeOne :: [a] -> [[a]]
+removeOne xs = [take i xs ++ drop (i + 1) xs | i <- [0 .. length xs - 1]]
+
 solvePart1 :: String -> String
-solvePart1 input = "unimplemented"
+solvePart1 input =
+  show . length . filter isSafe $ parseInput input
 
 solvePart2 :: String -> String
-solvePart2 input = "unimplemented"
+solvePart2 input =
+  show . length . filter (any isSafe . removeOne) $ parseInput input
