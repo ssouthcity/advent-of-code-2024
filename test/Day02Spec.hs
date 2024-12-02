6112f761c2a1413c9cb845e1e82d0138242109ce
ssouthcity <51554341+ssouthcity@users.noreply.github.com>
2024-12-02 23:53:51 +0100

    feat: complete day 2 puzzle (#3)

--- test/Day02Spec.hs
+++ test/Day02Spec.hs
@@ -3,16 +3,27 @@ module Day02Spec (tests) where
 import qualified Day02
 import Test.HUnit
 
+sample :: String
+sample =
+  unlines
+    [ "7 6 4 2 1",
+      "1 2 7 8 9",
+      "9 7 6 2 1",
+      "1 3 2 4 5",
+      "8 6 4 4 1",
+      "1 3 6 7 9"
+    ]
+
 testPart1 :: Test
 testPart1 =
-  let input = ""
-      answer = "unimplemented"
+  let input = sample
+      answer = "2"
    in TestCase $ assertEqual "" answer $ Day02.solvePart1 input
 
 testPart2 :: Test
 testPart2 =
-  let input = ""
-      answer = "unimplemented"
+  let input = sample
+      answer = "4"
    in TestCase $ assertEqual "" answer $ Day02.solvePart2 input
 
 tests :: Test
