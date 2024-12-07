0dc0df5fcff17036db72cefc0dc067e9fc2f8b46
Stian Sørby <51554341+ssouthcity@users.noreply.github.com>
2024-12-07 16:51:13 +0100

    wip

--- test/Day06Spec.hs
+++ test/Day06Spec.hs
@@ -3,16 +3,31 @@ module Day06Spec (tests) where
 import qualified Day06
 import Test.HUnit
 
+sample :: String
+sample =
+  unlines
+    [ "....#.....",
+      ".........#",
+      "..........",
+      "..#.......",
+      ".......#..",
+      "..........",
+      ".#..^.....",
+      "........#.",
+      "#.........",
+      "......#..."
+    ]
+
 testPart1 :: Test
 testPart1 =
-  let input = ""
-      answer = "unimplemented"
+  let input = sample
+      answer = "41"
    in TestCase $ assertEqual "" answer $ Day06.solvePart1 input
 
 testPart2 :: Test
 testPart2 =
-  let input = ""
-      answer = "unimplemented"
+  let input = sample
+      answer = "6"
    in TestCase $ assertEqual "" answer $ Day06.solvePart2 input
 
 tests :: Test
