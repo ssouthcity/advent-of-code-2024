923241b848b53beb5bd005b2d0f665faa341dbd7
ssouthcity <51554341+ssouthcity@users.noreply.github.com>
2024-12-07 18:57:42 +0100

    feat: complete day 7 puzzle (#7)

--- src/Day07.hs
+++ src/Day07.hs
@@ -1,7 +1,41 @@
 module Day07 where
 
+import Data.Attoparsec.Text
+import Data.Either (fromRight)
+import Data.Text (pack)
+
+type Operator = Int -> Int -> Int
+
+type Equation = (Int, [Int])
+
+cat :: Operator
+cat x y = read (show x ++ show y)
+
+parseEquation :: Parser Equation
+parseEquation = do
+  answer <- decimal
+  _ <- char ':'
+  _ <- char ' '
+  members <- decimal `sepBy` char ' '
+  return (answer, members)
+
+parseEquations :: Parser [Equation]
+parseEquations = parseEquation `sepBy` endOfLine
+
+mustParseEquations :: String -> [Equation]
+mustParseEquations = fromRight (error "Unexpected parsing failure") . parseOnly parseEquations . pack
+
+isCalibrateable :: [Operator] -> Equation -> Bool
+isCalibrateable _ (_, []) = False
+isCalibrateable _ (x, [y]) = x == y
+isCalibrateable ops (answer, x : y : xs) =
+  any (\op -> isCalibrateable ops (answer, op x y : xs)) ops
+
+totalCalibrationResult :: [Operator] -> [Equation] -> Int
+totalCalibrationResult ops = sum . map fst . filter (isCalibrateable ops)
+
 solvePart1 :: String -> String
-solvePart1 input = "unimplemented"
+solvePart1 = show . totalCalibrationResult [(+), (*)] . mustParseEquations
 
 solvePart2 :: String -> String
-solvePart2 input = "unimplemented"
+solvePart2 = show . totalCalibrationResult [(+), (*), cat] . mustParseEquations
--- test/Day07Spec.hs
+++ test/Day07Spec.hs
@@ -3,16 +3,30 @@ module Day07Spec (tests) where
 import qualified Day07
 import Test.HUnit
 
+sample :: String
+sample =
+  unlines
+    [ "190: 10 19",
+      "3267: 81 40 27",
+      "83: 17 5",
+      "156: 15 6",
+      "7290: 6 8 6 15",
+      "161011: 16 10 13",
+      "192: 17 8 14",
+      "21037: 9 7 18 13",
+      "292: 11 6 16 20"
+    ]
+
 testPart1 :: Test
 testPart1 =
-  let input = ""
-      answer = "unimplemented"
+  let input = sample
+      answer = "3749"
    in TestCase $ assertEqual "" answer $ Day07.solvePart1 input
 
 testPart2 :: Test
 testPart2 =
-  let input = ""
-      answer = "unimplemented"
+  let input = sample
+      answer = "11387"
    in TestCase $ assertEqual "" answer $ Day07.solvePart2 input
 
 tests :: Test
