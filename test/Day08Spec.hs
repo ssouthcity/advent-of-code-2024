5b4fdc14e1efabb29e82f1c1a37a44cf3dec34c1
Stian Sørby <51554341+ssouthcity@users.noreply.github.com>
2024-12-08 23:39:58 +0100

    feat: complete day 8 puzzle

--- test/Day08Spec.hs
+++ test/Day08Spec.hs
@@ -3,16 +3,33 @@ module Day08Spec (tests) where
 import qualified Day08
 import Test.HUnit
 
+sample :: String
+sample =
+  unlines
+    [ "............",
+      "........0...",
+      ".....0......",
+      ".......0....",
+      "....0.......",
+      "......A.....",
+      "............",
+      "............",
+      "........A...",
+      ".........A..",
+      "............",
+      "............"
+    ]
+
 testPart1 :: Test
 testPart1 =
-  let input = ""
-      answer = "unimplemented"
+  let input = sample
+      answer = "14"
    in TestCase $ assertEqual "" answer $ Day08.solvePart1 input
 
 testPart2 :: Test
 testPart2 =
-  let input = ""
-      answer = "unimplemented"
+  let input = sample
+      answer = "34"
    in TestCase $ assertEqual "" answer $ Day08.solvePart2 input
 
 tests :: Test
