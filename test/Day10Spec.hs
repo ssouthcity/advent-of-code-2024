df7ca3fa5b9043c8bbccf6872b8bd5b0331ea5d6
ssouthcity <51554341+ssouthcity@users.noreply.github.com>
2024-12-10 19:04:12 +0100

    feat: complete day 10 (#10)

--- test/Day10Spec.hs
+++ test/Day10Spec.hs
@@ -3,16 +3,29 @@ module Day10Spec (tests) where
 import qualified Day10
 import Test.HUnit
 
+sample :: String
+sample =
+  unlines
+    [ "89010123",
+      "78121874",
+      "87430965",
+      "96549874",
+      "45678903",
+      "32019012",
+      "01329801",
+      "10456732"
+    ]
+
 testPart1 :: Test
 testPart1 =
-  let input = ""
-      answer = "unimplemented"
+  let input = sample
+      answer = "36"
    in TestCase $ assertEqual "" answer $ Day10.solvePart1 input
 
 testPart2 :: Test
 testPart2 =
-  let input = ""
-      answer = "unimplemented"
+  let input = sample
+      answer = "81"
    in TestCase $ assertEqual "" answer $ Day10.solvePart2 input
 
 tests :: Test
