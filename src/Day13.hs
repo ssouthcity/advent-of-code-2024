3514c0cf92e20fcd1440c4a48a2f3d5e87ea95f3
Stian Sørby <51554341+ssouthcity@users.noreply.github.com>
2024-12-14 23:43:33 +0100

    feat: complete day 13 puzzle

--- src/Day13.hs
+++ src/Day13.hs
@@ -1,7 +1,83 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# OPTIONS_GHC -Wno-type-defaults #-}
+
 module Day13 where
 
+import Control.Applicative
+import Data.Attoparsec.Text
+import Data.Text (pack)
+
+type Vec2 = (Int, Int)
+
+data Game = Game
+  { buttonA :: Vec2,
+    buttonB :: Vec2,
+    prize :: Vec2
+  }
+  deriving (Show, Eq)
+
+vec2Parser :: Parser Vec2
+vec2Parser = do
+  _ <- char 'X'
+  _ <- char '+' <|> char '='
+  x <- decimal
+  _ <- string ", Y"
+  _ <- char '+' <|> char '='
+  y <- decimal
+  return (x, y)
+
+gameParser :: Parser Game
+gameParser = do
+  _ <- string "Button A: "
+  a <- vec2Parser
+  _ <- endOfLine
+
+  _ <- string "Button B: "
+  b <- vec2Parser
+  _ <- endOfLine
+
+  _ <- string "Prize: "
+  p <- vec2Parser
+  _ <- endOfLine
+
+  return $ Game a b p
+
+gamesParser :: Parser [Game]
+gamesParser = gameParser `sepBy` skipMany1 endOfLine
+
+crossProduct :: Vec2 -> Vec2 -> Int
+crossProduct (ax, ay) (bx, by) = ay * bx - ax * by
+
+factorize :: Vec2 -> Vec2 -> Vec2 -> Maybe Vec2
+factorize a@(ax, _) b@(bx, _) p@(px, _)
+  | r == 0 = Just (x, y)
+  | otherwise = Nothing
+  where
+    (y, r1) = crossProduct a p `divMod` crossProduct a b
+    (x, r2) = (px - bx * y) `divMod` ax
+    r = r1 + r2
+
+factorizeGame :: Game -> Maybe Vec2
+factorizeGame game = factorize (buttonA game) (buttonB game) (prize game)
+
+sumOfGame :: Maybe Vec2 -> Int
+sumOfGame v = case v of
+  Just (x, y) -> x * 3 + y
+  Nothing -> 0
+
+increasePrizes :: Game -> Game
+increasePrizes game = game {prize = (x + 10 ^ 13, y + 10 ^ 13)}
+  where
+    (x, y) = prize game
+
 solvePart1 :: String -> String
-solvePart1 input = "unimplemented"
+solvePart1 input =
+  case parseOnly gamesParser (pack input) of
+    Left err -> show $ "Parsing error: " ++ err
+    Right game -> show $ sum $ map (sumOfGame . factorizeGame) game
 
 solvePart2 :: String -> String
-solvePart2 input = "unimplemented"
+solvePart2 input =
+  case parseOnly gamesParser (pack input) of
+    Left err -> show $ "Parsing error: " ++ err
+    Right game -> show $ sum $ map (sumOfGame . factorizeGame . increasePrizes) game
--- test/Day13Spec.hs
+++ test/Day13Spec.hs
@@ -3,16 +3,36 @@ module Day13Spec (tests) where
 import qualified Day13
 import Test.HUnit
 
+sample :: String
+sample =
+  unlines
+    [ "Button A: X+94, Y+34",
+      "Button B: X+22, Y+67",
+      "Prize: X=8400, Y=5400",
+      "",
+      "Button A: X+26, Y+66",
+      "Button B: X+67, Y+21",
+      "Prize: X=12748, Y=12176",
+      "",
+      "Button A: X+17, Y+86",
+      "Button B: X+84, Y+37",
+      "Prize: X=7870, Y=6450",
+      "",
+      "Button A: X+69, Y+23",
+      "Button B: X+27, Y+71",
+      "Prize: X=18641, Y=10279"
+    ]
+
 testPart1 :: Test
 testPart1 =
-  let input = ""
-      answer = "unimplemented"
+  let input = sample
+      answer = "480"
    in TestCase $ assertEqual "" answer $ Day13.solvePart1 input
 
 testPart2 :: Test
 testPart2 =
-  let input = ""
-      answer = "unimplemented"
+  let input = sample
+      answer = "875318608908"
    in TestCase $ assertEqual "" answer $ Day13.solvePart2 input
 
 tests :: Test
