bcd4cc22398988f26e4d1bf23de0caa20abe67b0
Stian Sørby <stian.sorby@norges-bank.no>
2024-12-16 21:56:56 +0000

    feat: complete day 15 part 1

--- src/Day15.hs
+++ src/Day15.hs
@@ -1,7 +1,68 @@
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE NamedFieldPuns #-}
+
 module Day15 where
 
+import Data.Set (Set)
+import qualified Data.Set as Set
+import Vec2 (Vec2)
+import qualified Vec2
+
+data Warehouse = Warehouse
+  { robot :: Vec2,
+    boxes :: Set Vec2,
+    walls :: Set Vec2
+  }
+  deriving (Show)
+
+parseWarehouse :: [String] -> Warehouse
+parseWarehouse grid = Warehouse {robot, boxes, walls}
+  where
+    chars = [((x, y), c) | (y, row) <- zip [0 ..] grid, (x, c) <- zip [0 ..] row]
+    robot = head [pos | (pos, c) <- chars, c == '@']
+    boxes = Set.fromList [pos | (pos, c) <- chars, c == 'O']
+    walls = Set.fromList [pos | (pos, c) <- chars, c == '#']
+
+parseInstructions :: String -> [Vec2]
+parseInstructions = map $ \case
+  '^' -> Vec2.north
+  '>' -> Vec2.east
+  'v' -> Vec2.south
+  '<' -> Vec2.west
+  _ -> Vec2.zero
+
+parseInput :: String -> (Warehouse, [Vec2])
+parseInput input =
+  let (top, bottom) = break null (lines input)
+      warehouse = parseWarehouse top
+      instructions = parseInstructions (unlines $ drop 1 bottom)
+   in (warehouse, instructions)
+
+tryMoveBox :: Warehouse -> Vec2 -> Vec2 -> (Warehouse, Bool)
+tryMoveBox warehouse box direction
+  | box' `Set.member` walls warehouse = (warehouse, False)
+  | box' `Set.member` boxes warehouse = tryMoveBox warehouse box direction'
+  | otherwise = (warehouse {boxes = boxes'}, True)
+  where
+    box' = Vec2.add box direction
+    boxes' = Set.insert box' $ Set.delete box $ boxes warehouse
+    direction' = Vec2.add direction (Vec2.normalize direction)
+
+performInstruction :: Warehouse -> Vec2 -> Warehouse
+performInstruction warehouse instruction
+  | robot' `Set.member` walls warehouse = warehouse
+  | robot' `Set.member` boxes warehouse =
+      let (warehouse', ok) = tryMoveBox warehouse robot' instruction
+       in if ok then warehouse' {robot = robot'} else warehouse
+  | otherwise = warehouse {robot = robot'}
+  where
+    robot' = Vec2.add (robot warehouse) instruction
+
+lanternFishSum :: Warehouse -> Int
+lanternFishSum = sum . Set.map (\(x, y) -> 100 * y + x) . boxes
+
 solvePart1 :: String -> String
-solvePart1 input = "unimplemented"
+solvePart1 = show . lanternFishSum . uncurry (foldl performInstruction) . parseInput
 
 solvePart2 :: String -> String
 solvePart2 input = "unimplemented"
--- test/Day15Spec.hs
+++ test/Day15Spec.hs
@@ -1,12 +1,54 @@
 module Day15Spec (tests) where
 
+import Data.List (unlines)
 import qualified Day15
 import Test.HUnit
 
+sample :: String
+sample =
+  unlines
+    [ "##########",
+      "#..O..O.O#",
+      "#......O.#",
+      "#.OO..O.O#",
+      "#..O@..O.#",
+      "#O#..O...#",
+      "#O..O..O.#",
+      "#.OO.O.OO#",
+      "#....O...#",
+      "##########",
+      "",
+      "<vv>^<v^>v>^vv^v>v<>v^v<v<^vv<<<^><<><>>v<vvv<>^v^>^<<<><<v<<<v^vv^v>^",
+      "vvv<<^>^v^^><<>>><>^<<><^vv^^<>vvv<>><^^v>^>vv<>v<<<<v<^v>^<^^>>>^<v<v",
+      "><>vv>v^v^<>><>>>><^^>vv>v<^^^>>v^v^<^^>v^^>v^<^v>v<>>v^v^<v>v^^<^^vv<",
+      "<<v<^>>^^^^>>>v^<>vvv^><v<<<>^^^vv^<vvv>^>v<^^^^v<>^>vvvv><>>v^<<^^^^^",
+      "^><^><>>><>^^<<^^v>>><^<v>^<vv>>v>>>^v><>^v><<<<v>>v<v<v>vvv>^<><<>^><",
+      "^>><>^v<><^vvv<^^<><v<<<<<><^v<<<><<<^^<v<^^^><^>>^<v^><<<^>>^v<v^v<v^",
+      ">^>>^v>vv>^<<^v<>><<><<v<<v><>v<^vv<<<>^^v^>^^>>><<^v>>v^v><^^>>^<>vv^",
+      "<><^^>^^^<><vvvvv^v<v<<>^v<v>v<<^><<><<><<<^^<<<^<<>><<><^^^>^^<>^>v<>",
+      "^^>vv<^v^v<vv>^<><v<^v>^^^>>>^^vvv^>vvv<>>>^<^>>>>>^<<^v>^vvv<>^<><<v>",
+      "v^^>>><<^^<>>^v^<v^vv<>v^<<>^<^v^v><^<<<><<^<v><v<>vv>>v><v^<vv<>v^<<^"
+    ]
+
+tinySample :: String
+tinySample =
+  unlines
+    [ "########",
+      "#..O.O.#",
+      "##@.O..#",
+      "#...O..#",
+      "#.#.O..#",
+      "#...O..#",
+      "#......#",
+      "########",
+      "",
+      "<^^>>>vv<v>>v<<"
+    ]
+
 testPart1 :: Test
 testPart1 =
-  let input = ""
-      answer = "unimplemented"
+  let input = sample
+      answer = "10092"
    in TestCase $ assertEqual "" answer $ Day15.solvePart1 input
 
 testPart2 :: Test
