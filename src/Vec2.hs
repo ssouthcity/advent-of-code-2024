25b9514288ce05a75f454614a3e7b1f4d477a4c5
Stian Sørby <stian.sorby@norges-bank.no>
2024-12-18 13:25:55 +0000

    feat: add vec2 library

---
+++ src/Vec2.hs
@@ -0,0 +1,37 @@
+{-# OPTIONS_GHC -Wno-type-defaults #-}
+
+module Vec2 where
+
+type Vec2 = (Int, Int)
+
+zero :: Vec2
+zero = (0, 0)
+
+north :: Vec2
+north = (0, -1)
+
+east :: Vec2
+east = (1, 0)
+
+south :: Vec2
+south = (0, 1)
+
+west :: Vec2
+west = (-1, 0)
+
+add :: Vec2 -> Vec2 -> Vec2
+add (x, y) (x', y') = (x + x', y + y')
+
+mult :: Vec2 -> Vec2 -> Vec2
+mult (x, y) (x', y') = (x * x', y * y')
+
+normalize :: Vec2 -> Vec2
+normalize (x, y) = (normalize' x, normalize' y)
+  where
+    normalize' n
+      | n > 0 = 1
+      | n < 0 = -1
+      | otherwise = 0
+
+distance :: Vec2 -> Vec2 -> Float
+distance (x, y) (x', y') = sqrt $ fromIntegral ((x - x') ^ 2 + (y - y') ^ 2)
