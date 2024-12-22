82994956905b6e9bc39c0c00df02f9ccd2de2e4a
Stian Sørby <51554341+ssouthcity@users.noreply.github.com>
2024-12-22 22:07:43 +0100

    feat: complete day 22 puzzle

--- src/Day22.hs
+++ src/Day22.hs
@@ -1,7 +1,45 @@
 module Day22 where
 
+import Data.Bits (xor)
+import Data.Function (on)
+import Data.List (groupBy, sortOn, tails)
+import qualified Data.Map as Map
+
+mix :: Int -> Int -> Int
+mix x y = x `xor` y
+
+prune :: Int -> Int
+prune x = x `mod` magicPruneNum
+  where
+    magicPruneNum = 16777216
+
+generateSecretNumber :: Int -> Int
+generateSecretNumber x = x'''
+  where
+    x' = prune $ mix (x * 64) x
+    x'' = prune $ mix (x' `div` 32) x'
+    x''' = prune $ mix (x'' * 2048) x''
+
+lastDigit :: Int -> Int
+lastDigit n = abs n `mod` 10
+
+diffs :: [Int] -> [Int]
+diffs xs = zipWith (-) (tail xs) xs
+
+groupsOfN :: Int -> [a] -> [[a]]
+groupsOfN n xs = [take n (drop x xs) | x <- [0 .. length xs - n]]
+
+zipWithDiffs :: [Int] -> [([Int], Int)]
+zipWithDiffs xs = zip (replicate 4 [] ++ map (take 4) (tails (diffs xs))) xs
+
+sumSameSequences :: [(Char, Int)] -> [(Char, Int)]
+sumSameSequences xs = map sumGroup grouped
+  where
+    grouped = groupBy ((==) `on` fst) (sortOn fst xs)
+    sumGroup grp = (fst (head grp), sum (map snd grp))
+
 solvePart1 :: String -> String
-solvePart1 input = "unimplemented"
+solvePart1 = show . sum . map (last . take 2001 . iterate generateSecretNumber . read) . lines
 
 solvePart2 :: String -> String
-solvePart2 input = "unimplemented"
+solvePart2 = show . maximum . foldr (Map.unionWith (+) . Map.delete [] . Map.fromList . reverse . zipWithDiffs . map lastDigit . take 2001 . iterate generateSecretNumber . read) Map.empty . lines
--- src/Input.hs
+++ src/Input.hs
@@ -1,6 +1,9 @@
-module Input (readInputForDay) where
+module Input (readInputForDay, readSampleInputForDay) where
 
 import Text.Printf (printf)
 
 readInputForDay :: Int -> IO String
 readInputForDay day = readFile $ printf "input/day%02d.txt" day
+
+readSampleInputForDay :: Int -> IO String
+readSampleInputForDay = readFile . printf "input/day%02d.sample.txt"
